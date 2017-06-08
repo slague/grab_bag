e3a59119de5ad19bce022e18876bf5fdc4af7f1e
Seth <seth19483@gmail.com>
2017-06-08 09:09:07 -0600

    redirect back

--- app/controllers/users/folders/binaries/file_likes_controller.rb
+++ app/controllers/users/folders/binaries/file_likes_controller.rb
@@ -5,7 +5,7 @@ class Users::Folders::Binaries::FileLikesController < ApplicationController
       if !@binary.likes.find_by(user_id: current_user.id)
         @like = @binary.likes.create(user: current_user)
       end
-      redirect_to @binary.url
+      redirect_to :back
     end
   end
 
@@ -13,7 +13,7 @@ class Users::Folders::Binaries::FileLikesController < ApplicationController
     if current_user
       @binary = Binary.find_by(name: params[:binary_name])
       @binary.likes.find_by(user: current_user).destroy
-      redirect_to @binary.url
+      redirect_to :back
     end
   end
 end
